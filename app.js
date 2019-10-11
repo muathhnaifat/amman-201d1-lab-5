705f6f420b52bd2c2a6eba11a651556fefe8488b
Muath <muath_hnaifat@yahoo.com>
2019-10-11 21:00:28 +0300

    function3

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-// function sum(a, b) { //eslint-disable-line
-// var c= a+b;
-// return [c, "The sum of " + a + " and " + b + " is " + c + "."];
-// }
+function sum(a, b) { //eslint-disable-line
+var c= a+b;
+return [c, "The sum of " + a + " and " + b + " is " + c + "."];
+}
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
 
@@ -26,9 +26,9 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(x, y) { 
-    var z = x*y;
-    return [z, "The product of " + x + " and " + y + " is " + z + "." ] ;
+function multiply(a, b) { 
+    var c = a*b;
+    return [c, "The product of " + a + " and " + b + " is " + c + "." ] ;
     }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -47,12 +47,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
-// Write your code here
+// Write your code here 
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var sum1= sum(a,b);
+   var sum2= sum(sum1[0],c)
+    var multip1 =multiply(a,b);
+    var multip2=multiply(multip1[0],c)
+  return [sum2[0],
+  multip2[0],
+    'the sum of' + a + " and " + b + " and " + c + " sum to " + sum2[0] +".",
+   "The product of " + a + " and " + b + " and " + c + " is " + multip2[0] + ".",
+  
+];
+  }
 
-}
-
-// Here is the test for sumAndMultiply(); uncomment it to run it
 // testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
