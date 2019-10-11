b49f0c7ff504d71c7f6ff7bd1e79546d998002f2
Muath <muath_hnaifat@yahoo.com>
2019-10-11 18:34:01 +0300

    function1

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
-}
-
+    var c= a+b;
+    return [c, "The sum of " + a + " and " + b + " is " + c + "."];
+    }
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
 
